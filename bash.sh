26762d1ac07e72b6fac761c3504e47d0c679b0ad
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-03-19 01:47:57 +1000

    parallel data processing

--- bash.sh
+++ bash.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-export OCSSWROOT=/home/kitfloppa/ocssw
+export OCSSWROOT=/home/floppa/ocssw
 source $OCSSWROOT/OCSSW_bash.env
 
-file=$1
-path="/mnt/d/Andrey/FEFU/Dissertation/Data/MODIS_ndvi_250"
+function process_l2() {
+    file=$(echo $1|tr -d '\r')
+    mkdir ../Data/"${file::-4}"
+    cd ../Data/"${file::-4}"
 
-curl -O -b ~/.urs_cookies -c ~/.urs_cookies -L -n "https://oceandata.sci.gsfc.nasa.gov/ob/getfile/$file.bz2"
-bzip2 -d "$file.bz2"
+    curl -O -b ~/.urs_cookies -c ~/.urs_cookies -L -n "https://oceandata.sci.gsfc.nasa.gov/ob/getfile/$file.bz2"
+    bzip2 -d "$file.bz2"
 
-~/ocssw/bin/modis_L1A "$file" -o "$path/$file.L1A" -m aqua
-~/ocssw/bin/modis_GEO "$path/$file.L1A" -o "$path/$file.GEO"
-~/ocssw/bin/modis_L1B "$path/$file.L1A" -o "$path/$file.L1B_1KM" -k "$path/$file.L1B_HKM" -q "$path/$file.L1B_QKM"
-~/ocssw/bin/l2gen ifile="$path/$file.L1B_1KM" geofile="$path/$file.GEO" ofile="$path/$file.L2" maskland=off resolution=250 l2prod=ndvi
+    ~/ocssw/bin/modis_L1A "$file" -o "$file.L1A" -m aqua
+    ~/ocssw/bin/modis_GEO "$file.L1A" -o "$file.GEO"
+    ~/ocssw/bin/modis_L1B "$file.L1A" -o "$file.L1B_1KM" -k "$file.L1B_HKM" -q "$file.L1B_QKM"
+    ~/ocssw/bin/l2gen ifile="$file.L1B_1KM" geofile="$file.GEO" ofile="$file.L2" maskland=off resolution=250 l2prod=ndvi
 
-rm "$path/$file.L1A"
-rm "$path/$file.GEO"
-rm "$path/$file.L1B_1KM"
-rm "$path/$file.L1B_HKM"
-rm "$path/$file.L1B_QKM"
+    find . -type f -name 'MCFWrite*' -exec rm -f {} +
+
+    rm "$file"
+    rm "$file.L1A"
+    rm "$file.GEO"
+    rm "$file.L1B_1KM"
+    rm "$file.L1B_HKM"
+    rm "$file.L1B_QKM"
+}
+export -f process_l2
+
+parallel -j 8 -a test.txt process_l2
