1eb9d6b96bf7e9618a6764c278b4cd18a3e99135
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-03-23 17:27:21 +1000

    add station files

--- 123.py
+++
@@ -1,3 +0,0 @@
-string = '01.01.2021'
-
-print(string[6:10])
--- U321.sh
+++
@@ -1,6 +0,0 @@
-
-file=$1
-
-cd ../NDVI_Data/"NDVI_[06.01.2021]"/Data
-
-find ../../../LCFR-2021 -type f -name "LCFR01_${file:7:4}_${file:11:3}*" -exec cp {} . \;
